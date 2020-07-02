637c3e00b9dbb1d305786efc9d48088df2c5f1a6
loibq1993 <buiquangloi1993@gmail.com>
2020-07-02 11:37:32 +0700

    upload s3

--- server/helper/upload.js
+++ server/helper/upload.js
@@ -1,17 +1,38 @@
+const AWS = require('aws-sdk')
 const fs = require('fs');
-const path = require('path');
-const dirPath = path.join(__dirname, '../public/images/');
+const Bucket = 'node-demo-project';
+// const dirPath = path.join(__dirname, '../public/images/');
+
+// Enter copied or downloaded access ID and secret key here
+const ID = process.env.ID;
+const SECRET = process.env.SECRET;
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
 
 export function upload(image) {
     let imgName = image.originalname.split('.').slice(0, -1).join('.');
     let newName =  imgName + '_' + Date.now() + '.' + image.originalname.split('.').pop();
-    fs.writeFile(dirPath + newName, image.buffer, function(err){
-        if (err) throw err;
-    })
+    console.log(Buffer.from(image.buffer, 'base64'));
+    const params = {
+        Bucket: Bucket,
+        Key: newName, // File name you want to save as in S3
+        Body: Buffer.from(image.buffer, 'base64'),
+        ContentType:image.mimetype,
+        ACL: 'public-read',
+    };
+    s3.putObject(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+    });
     return newName;
 }
 
 export function deleteImage(image) {
-    let imagePath = dirPath + image
-    if (fs.existsSync(imagePath)) return fs.unlinkSync(imagePath)
+    s3.deleteObject({
+        Bucket: Bucket,
+        Key: image
+      },function (err,data){})
 }
